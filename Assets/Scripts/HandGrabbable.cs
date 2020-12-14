8c6bdc383b6c78f54656d8f39c46f4661375d012
matthewhallberg <matthewhallberg@gmail.com>
2020-12-14 02:23:38 -0500

    added moving and scaling, removed unused folders.

---
+++ Assets/Scripts/HandGrabbable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HandGrabbable : OVRGrabbable {
+
+    const float PINCH = .7f;
+    const float ROTATE_SPEED = 6f;
+
+    Quaternion desiredRotation;
+    bool canRotate;
+    float lastScaleDelta = -1;
+    List<OVRHand> hands;
+    int currHandCount;
+
+    protected override void Start() {
+        base.Start();
+        desiredRotation = transform.rotation;
+        hands = FindObjectsOfType<OVRHand>().ToList();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        OVRHand hand = other.GetComponent<OVRHand>();
+        if (hand != null) {
+            currHandCount++;
+        }
+        lastScaleDelta = -1;
+    }
+
+    void OnTriggerExit(Collider other) {
+        OVRHand hand = other.GetComponent<OVRHand>();
+        if (hand != null) {
+            currHandCount--;
+        }
+    }
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint) {
+        base.GrabBegin(hand, grabPoint);
+        canRotate = true;
+    }
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity) {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        canRotate = false;
+        SetDesiredRotation();
+    }
+
+    void Update() {
+        if (!canRotate) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * ROTATE_SPEED);
+        }
+
+        if (currHandCount == 2) {
+            float firstPinch = hands[0].GetFingerPinchStrength(OVRHand.HandFinger.Index);
+            float secondPinch = hands[1].GetFingerPinchStrength(OVRHand.HandFinger.Index);
+
+            if (firstPinch > PINCH && secondPinch > PINCH) {
+                float currScaleDelta = Vector3.Distance(hands[0].transform.position, hands[1].transform.position);
+                if (lastScaleDelta != -1) {
+                    float currScaleAmount = currScaleDelta - lastScaleDelta;
+                    transform.localScale += Vector3.one * currScaleAmount;
+                }
+                lastScaleDelta = currScaleDelta;
+            }
+        }
+    }
+
+    void SetDesiredRotation() {
+        //only allow y axis rotation
+        Vector3 currAngle = transform.eulerAngles;
+        currAngle.x = 0;
+        currAngle.z = 0;
+        desiredRotation = Quaternion.Euler(currAngle);
+    }
+}
---
+++ Assets/Scripts/HandGrabber.cs
@@ -0,0 +1,25 @@
+﻿public class HandGrabber : OVRGrabber {
+
+    const float PINCH = .7f;
+
+    OVRHand hand;
+
+    protected override void Start() {
+        base.Start();
+        hand = GetComponent<OVRHand>();
+    }
+
+     public override void Update() {
+        base.Update();
+        DetectPinch();
+    }
+
+    void DetectPinch() {
+        float currPinch = hand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+        if (!m_grabbedObj && currPinch > PINCH) {
+            GrabBegin();
+        } else if (m_grabbedObj && currPinch < PINCH) {
+            GrabEnd();
+        }
+    }
+}
