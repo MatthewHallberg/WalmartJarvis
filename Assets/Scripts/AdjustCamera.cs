8691cffde0f2306edf9614da15efe83cd8f98448
matthewhallberg <matthewhallberg@gmail.com>
2020-12-18 02:56:11 -0500

    added adjustment to camera, screen recording looks better now, slightly...

--- Assets/Scripts/AdjustCamera.cs
+++ Assets/Scripts/AdjustCamera.cs
@@ -5,17 +5,21 @@ public class AdjustCamera : MonoBehaviour {
     public Transform leftCam;
     public Transform rightCam;
 
-    [Header("X-SHIFT")]
-    [Range(1, 6f)]
-    public float X;
+    [Header("Eye Distance")]//2.61
+    [Range(2.2f, 2.8f)]
+    public float eyeDistance;
 
-    [Header("Y-SHIFT")]
+    [Header("X-Shift")]
+    [Range(-1f, 1f)]
+    public float xShift;
+
+    [Header("Y-SHIFT")]//-.053
     [Range(-.4f, .4f)]
-    public float Y;
+    public float yShift;
 
-    [Header("Z-SHIFT")]
-    [Range(0f, -5f)]
-    public float Z;
+    [Header("Z-SHIFT")]//-1
+    [Range(-2f, 0f)]
+    public float zShift;
 
     void Start() {
         LoadValues();
@@ -30,22 +34,25 @@ public class AdjustCamera : MonoBehaviour {
     }
 
     void SetValues() {
-        //leftCam.localPosition = new Vector3(-X, Y, Z);
-        //rightCam.localPosition = new Vector3(X, Y, Z);
+        leftCam.localPosition = new Vector3(-eyeDistance + xShift, yShift, zShift);
+        rightCam.localPosition = new Vector3(eyeDistance + xShift, yShift, zShift);
     }
 
     void LoadValues() {
         if (PlayerPrefs.HasKey("x")) {
-            X = PlayerPrefs.GetFloat("x");
-            Y = PlayerPrefs.GetFloat("y");
-            Z = PlayerPrefs.GetFloat("z");
+            xShift = PlayerPrefs.GetFloat("x");
+            yShift = PlayerPrefs.GetFloat("y");
+            zShift = PlayerPrefs.GetFloat("z");
+            eyeDistance = PlayerPrefs.GetFloat("eyeDistance");
+
         }
         SetValues();
     }
 
     void SaveValues() {
-        //PlayerPrefs.SetFloat("x", X);
-        PlayerPrefs.SetFloat("y", Y);
-        //PlayerPrefs.SetFloat("z", Z);
+        PlayerPrefs.SetFloat("x", xShift);
+        PlayerPrefs.SetFloat("y", yShift);
+        PlayerPrefs.SetFloat("z", zShift);
+        PlayerPrefs.SetFloat("eyeDistance", eyeDistance);
     }
 }
